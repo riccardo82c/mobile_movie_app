f3419165b7bef003563e48753f572ab3c3fe6dad
Riccardo Colombo <riccardo.colombo@ext.wefox.com>
2025-03-12 18:14:39 +0100

    fetch movies and display images list

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,6 @@ import './globals.css'
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
   </Stack>
 }
